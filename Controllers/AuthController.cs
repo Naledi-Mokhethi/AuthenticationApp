9ed2ab5034ec0b43ba2e6a002c0f3a0fddf87044
Naledi-Mokhethi <naledimokhethi28@gmail.com>
2025-02-17 20:55:30 +0200

    Moving code from controller to services

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using AuthenticationApp.Models;
+using AuthenticationApp.Services;
 using AuthenticationApp.UserDTO;
 using Mapster;
 using Microsoft.AspNetCore.Http;
@@ -13,64 +14,24 @@ namespace AuthenticationApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController(IConfiguration configuration) : ControllerBase
+    public class AuthController(IAuthService authService) : ControllerBase
     {
-        //Static user, just to test out the API
-        //Seeing if we need the Dto
-        public static User user = new (); //testing only 
-
         [HttpPost("Register")]
-        public ActionResult<User> Register(UserDto request)
+        public async Task<ActionResult<User>> Register(UserDto request)
         {
-            var hashedPassword = new PasswordHasher<User>()
-                .HashPassword(user, request.EmployeePassword);
-            //mapping the data, will be moved obviously 
-            user.EmployeeName = request.EmployeeName;
-            user.EmployeeLastName = request.EmployeeLastName;
-            user.EmployeeDepartment = request.EmployeeDepartment;
-            user.EmloyeeeJobTitle = request.EmloyeeeJobTitle;
-            user.EmployeePhoneNumber = request.EmployeePhoneNumber;
-            user.EmployeeEmail = request.EmployeeEmail;
-            user.EmployeeHashedPassword = hashedPassword;
-            
-
+            var user = await authService.RegisterAsync(request);
             return Ok(user);
         }
         [HttpPost("Login")]
-        public ActionResult<string> Login(LoginUserDto request)
+        public async Task<ActionResult<string>> Login(LoginUserDto request)
         {
-            if(user.EmployeeEmail != request.EmployeeEmail)
+            var token = await authService.LoginAsync(request);
+            if (token == null)
             {
-                return BadRequest("User not found");
+                return BadRequest("");
             }
-            if(new PasswordHasher<User>().VerifyHashedPassword(user, user.EmployeeHashedPassword, request.EmployeePassword) 
-                == PasswordVerificationResult.Failed)
-            {
-                return BadRequest("Wrong passord");
-            }
-            string token = CreateToken(user);
             return Ok(token);
         }
 
-        private string CreateToken(User user)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Email, user.EmployeeEmail)
-
-             };
-            var key = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
-
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var tokenDescriptor = new JwtSecurityToken(
-                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
-                audience: configuration.GetValue<string>("AppSettings: Audiance"),
-                claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds
-                );
-            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
-        }
     }
 }
