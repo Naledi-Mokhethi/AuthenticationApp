7a41841de81bdb82eeea9729fe28b428bc8e145d
Naledi-Mokhethi <naledimokhethi28@gmail.com>
2025-02-17 20:56:23 +0200

    Adding interface

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -6,7 +6,7 @@ namespace AuthenticationApp.Services
     public interface IAuthService
     {
         Task<User?> RegisterAsync(UserDto request);
-        Task<string> LoginAsync(UserDto request);//Temp
+        Task<string> LoginAsync(LoginUserDto request);//Temp
        // Task<LoginUserDto> LoginAsync1(LoginUserDto loginRequest);//Perm
     }
 }
