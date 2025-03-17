91135a094246d368d88e467420a331c620014236
Naledi-Mokhethi <naledimokhethi28@gmail.com>
2025-03-17 21:29:08 +0200

    Configure API Authentication scheme

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 using AuthenticationApp.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +13,22 @@ builder.Services.AddControllers();
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 IConfiguration configuration = builder.Configuration;   
 
-//Add Authentication Sche
-
+//Add Authentication Scheme 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["AppSettings:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["AppSettings:Audience"],
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["AppSetting:Token"]!)),
+            ValidateIssuerSigningKey = true
+        };
+    });
 //Add Auth Service
 builder.Services.AddTransient<IAuthService, AuthService>(provider => new AuthService(connectionString!, configuration));
 
